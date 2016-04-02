408e4944bfede5360b8ad79a26f2de11960df540
Cef Ramirez <cef@Cefs-MacBook-Pro.local>
2016-04-02 15:55:55 -0700

    adding mdot serial driver

---
+++ app/src/main/java/com/cmu/ccgs/loranetworkmapper/lora/mdot/driver/UsbId.java
@@ -0,0 +1,11 @@
+package com.cmu.ccgs.loranetworkmapper.lora.mdot.driver;
+
+/**
+ * Created by cef on 3/25/16.
+ */
+public class UsbId {
+
+    public static final int VENDOR_ID = 0x0438;
+    public static final int PRODUCT_ID = 0x374B;
+
+}
